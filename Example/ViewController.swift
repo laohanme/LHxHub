ffeffe60b80044600c983c7dbed437be5be95b0d
laohame <jimocheng97@hotmal.com>
2020-05-06 14:05:00 +0800

    Add Example for LHxHub

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by laohanme on 06/05/2020.
+//  Copyright © 2020 laohanme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "LHxHub"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+}
+
